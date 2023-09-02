d78764de540009d417f2d18909fcae543c897c02
Jenipher <jenurefer2111@gmail.com>
2023-09-02 01:34:25 -0600

    footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <footer className="container-fluid bg-light ">
+    <div className="row mt-2 align-items-center">
+      <div className="col border-end">
+        <h2>SMILE</h2>
+        <i class="fa-solid fa-phone"></i> +506 2266-9988
+        <br />
+        <i class="fa-solid fa-envelope"></i> info@clinicadentalsmile.com
+        <p></p>
+      </div>
+      <div className="col border-end">
+        SOBRE CLINICA DENTAL SMILE
+        <br />
+        <i class="fa-solid fa-street-view"></i>
+        <br />
+        NUESTRAS OFICINAS
+      </div>
+      <div className="col border-end">
+        <i class="fa-brands fa-facebook-f"></i>
+        <br />
+        <i class="fa-brands fa-instagram"></i>
+        <br />
+        <i class="fa-brands fa-tiktok"></i>
+        <br />
+        <i class="fa-brands fa-youtube"></i>
+      </div>
+    </div>
+    <br />
+  </footer>
 );
