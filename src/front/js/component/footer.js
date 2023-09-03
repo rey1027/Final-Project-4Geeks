554b9c1f706a555285638005bdd52a51ae768eae
Jenipher <jenurefer2111@gmail.com>
2023-09-02 22:22:59 -0600

    Primer ventada de inicio

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,22 +2,21 @@ import React, { Component } from "react";
 
 export const Footer = () => (
   <footer className="container-fluid bg-light ">
-    <div className="row mt-2 align-items-center">
+    <div className="row mt-2 align-items-center ">
       <div className="col border-end">
-        <h2>SMILE</h2>
+        <h2 className="text-center">LOGO</h2>
         <i class="fa-solid fa-phone"></i> +506 2266-9988
         <br />
         <i class="fa-solid fa-envelope"></i> info@clinicadentalsmile.com
         <p></p>
       </div>
-      <div className="col border-end">
-        SOBRE CLINICA DENTAL SMILE
+      <div className="col border-end text-center">
         <br />
         <i class="fa-solid fa-street-view"></i>
         <br />
         NUESTRAS OFICINAS
       </div>
-      <div className="col border-end">
+      <div className="col border-end text-center ">
         <i class="fa-brands fa-facebook-f"></i>
         <br />
         <i class="fa-brands fa-instagram"></i>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,23 +4,52 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="container-fluid">
+      <div className="fondo inicio">
+        <div className="row d-flex">
+          <div className="col-7 mt-5 text-center">
+            <p>
+              <h1>
+                <strong>SOMOS</strong>
+              </h1>
+            </p>
+            <p>
+              La clínica dental Smile es un centro especializado en brindar
+              servicios odontológicos de excelencia y atención personalizada.
+              Nuestro equipo de profesionales altamente capacitados se dedica a
+              mejorar y mantener la salud bucal de nuestros pacientes, con un
+              enfoque en lograr sonrisas radiantes y saludables.
+            </p>
+          </div>
+          <div className="col-5">
+            <img
+              src="https://i.pinimg.com/564x/a0/42/03/a04203147caef4107b9a3e48af3e7f57.jpg"
+              className=" mx-auto d-block chica shadow-lg "
+            ></img>
+          </div>
+        </div>
+        <div className="contenedor shadow-lg">
+          <div className="row">
+            <div className="col-6 text-start">
+              <p>1</p>
+            </div>
+            <div className="col-6 text-center">
+              <p>2</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-6 text-start">
+              <p>3</p>
+            </div>
+            <div className="col-6 text-center">
+              <p>4</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
