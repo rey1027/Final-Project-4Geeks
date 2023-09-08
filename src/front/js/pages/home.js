4b95e091bf0097bda88811fdcd9e33cb6ef58811
Jenipher <jenurefer2111@gmail.com>
2023-09-07 23:34:41 -0600

    Especialistas y reservacion listo

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,6 +3,7 @@ import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import protesis from "../../img/protesis.png";
 import doc1 from "../../img/doc1.png";
+import g3 from "../../img/g3.png";
 import d5 from "../../img/d5.png";
 
 export const Home = () => {
@@ -12,7 +13,7 @@ export const Home = () => {
     <div className="container-fluid">
       <div className=" inicio">
         <div className="row d-flex">
-          <div className="col-7 SOMOS mt-5 text-center">
+          <div className="col-lg-7 col-sm-12 SOMOS mt-5 text-center">
             <p className="display-3">
               <strong>SOMOS</strong>
             </p>
@@ -24,7 +25,7 @@ export const Home = () => {
               enfoque en lograr sonrisas radiantes y saludables.
             </h3>
           </div>
-          <div className="col-5">
+          <div className="col-lg-5 col-sm-12 ">
             <img
               src="https://i.pinimg.com/564x/a0/42/03/a04203147caef4107b9a3e48af3e7f57.jpg"
               className=" mx-auto d-block chica shadow-lg "
@@ -33,16 +34,16 @@ export const Home = () => {
         </div>
         <div className="contenedor d-flex shadow-lg align-items-center">
           <div className="row">
-            <div className="col-1 p-4 text-white">
+            <div className="col-lg-1 col-sm-12 p-4 text-white">
               <h1>+10</h1>
               <h5>Especialidades</h5>
             </div>
-            <div className="col-6 text-start">
+            <div className="col-lg-6 col-sm-12 text-start">
               <div>
-                <img src={protesis} />
+                <img className="protesis" src={protesis} />
               </div>
             </div>
-            <div className="col-5">
+            <div className="col-lg-5 col-sm-12">
               <div className=" mt-5 text-md-start p-5 text-white fw-bol">
                 <h4>
                   {" "}
@@ -59,33 +60,82 @@ export const Home = () => {
       </div>
       <div className="container-fluid">
         <div className="row doctora">
-          <div className="col-1">
-            <div>
-              <img className="d5" src={d5} />
-            </div>
-          </div>
-          <div className="col-7 mt-5">
+          <div className="col-lg-6 col-sm-7 texto mt-5">
             <p>
               <h1 className="text-start text-white mt-5">
-                <strong>Especialistas</strong>
+                <strong>Nuestros Especialistas</strong>
               </h1>
             </p>
-            <div className="texto text-md-end text-white fw-bol">
+            <div className="text-start text-white fw-bol">
               <h4>
-                En nuestra clínica dental, contamos con un equipo de
-                especialistas altamente capacitados y dedicados a brindarle la
-                mejor atención odontológica. Están comprometidos con su salud
-                bucal y tienen una amplia experiencia en diversas áreas de la
-                odontología.
+                ¡Te invitamos a descubrir a nuestros especialistas en
+                odontología! En nuestra clínica dental, contamos con un equipo
+                de profesionales altamente capacitados y especializados en
+                diversas áreas de la odontología. Están comprometidos con
+                brindar la mejor atención dental.
               </h4>
               <a href="#" class=" boton1 btn btn-light mt-4">
                 Learn more
               </a>
             </div>
           </div>
-          <div className="col-2 text-end mt-5">
+          <div className="col-lg-6 col-sm-5 text-center mt-5">
             <div>
-              <img className="doc1 " src={doc1} />
+              <img className="doc1 rounded" src={doc1} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div className="container-fluid">
+        <div className="row especialidades">
+          <div className="col-lg-6 col-sm-5 text-center mt-5">
+            <div></div>
+          </div>
+          <div className="col-lg-6 textoD text-dark col-sm-7 mt-5">
+            <p>
+              <h1 className="text-start mt-5">
+                <strong>Especialidades</strong>
+              </h1>
+            </p>
+            <div className="text-start fw-bol">
+              <h4>
+                Estamos encantados de invitarte a explorar nuestras
+                especialidades en el campo de la odontología. En nuestra
+                clínica, nos enorgullece ofrecer una amplia gama de servicios
+                especializados para satisfacer todas tus necesidades dentales.
+              </h4>
+              <a href="#" class=" boton1 btn btn-light mt-4">
+                Learn more
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div className="container-fluid">
+        <div className="row solicitud">
+          <div className="col-lg-6 col-sm-5 text-center mt-5">
+            <div>
+              <img className="rounded" src={d5} />
+            </div>
+          </div>
+          <div className="col-lg-6 col-sm-7 mt-5">
+            <p>
+              <h1 className="text-start text-white mt-5">
+                <strong>Reservacion de cita</strong>
+              </h1>
+            </p>
+            <div className="text-start text-white fw-bol">
+              <h4>
+                Estamos encantados de invitarte a explorar nuestras
+                especialidades en el campo de la odontología. En nuestra
+                clínica, nos enorgullece ofrecer una amplia gama de servicios
+                especializados para satisfacer todas tus necesidades dentales.
+              </h4>
+              <a href="#" class=" boton1 btn btn-light mt-4">
+                Solicitud de cita
+              </a>
             </div>
           </div>
         </div>
