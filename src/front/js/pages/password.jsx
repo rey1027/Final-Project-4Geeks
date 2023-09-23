e67a2b357449aa31c1044519a0214f1138db9f24
glnd-drn <glendaduranc@hotmail.com>
2023-09-22 23:05:15 -0600

    No se conecta a la ruta creada /fprgot_password y además tira error a la pagina que se redirige /home

--- src/front/js/pages/password.jsx
+++ src/front/js/pages/password.jsx
@@ -1,100 +1,102 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/password.css";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
 
-const password = () => {
-    const [email, setEmail] = useState("");
-    const { store, actions } = useContext(Context);
-    let navigate = useNavigate(""); //
-  
-    const login = async () => {
-      /*if (email == "") {
-        Swal.fire({
-          icon: "warning",
-          title: "Cuidado!",
-          text: "Debes ingresar el correo ",
-          timer: 3500,
-        });
-      }*/
-      let obj = {
-        email: email,
-      };
-  
-      let response = await actions.fetchPromise(
-        "/api/inicio-sesion",
-        "POST",
-        obj
-      );
-  
-      if (response.ok) {
-        let responseJson = await response.json();
-        console.log(responseJson);
+const Password = () => {
+  const [email, setEmail] = useState("");
+  const { actions } = useContext(Context);
+  const navigate = useNavigate();
+
+  const login = () => {
+    if (email === "") {
+      Swal.fire({
+        icon: "warning",
+        title: "Cuidado!",
+        text: "Debes ingresar el correo",
+        timer: 3500,
+      });
+      return;
+    }
+
+    const obj = {
+      email: email,
+    };
+
+    fetch("/forgot_password", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(obj),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Error en la solicitud");
+        }
+      })
+      .then((responseData) => {
         Swal.fire({
           position: "center",
           icon: "success",
-          title: "Inicio sesión correctamente",
+          title: responseData.message,
           timer: 2500,
         });
-        localStorage.setItem("token", responseJson.token);
-        actions.activateLoginConfirmation();
-        navigate("/citas"); //Ruta a la que queremos ir
-      } else {
-        let responseJson = await response.json();
-        console.log(responseJson);
+        navigate("/inicio-sesion"); // Redirige a la ruta deseada
+      })
+      .catch((error) => {
+        console.error("Error en la solicitud:", error);
         Swal.fire({
           icon: "error",
-          title: responseJson.message,
-          text: "Error en la validación del usuario!",
+          title: "Error en la solicitud",
           timer: 3500,
         });
-      }
-      return;
-    };
-  
-    return (
-      <>
-        <div className="Login ">
-          <div className="row d-flex justify-content-center">
-            <h1 className="d-flex justify-content-center titulo "></h1>
-          </div>
-  
-          <div className="mb-3 row">
-            <div className="col-4"></div>
-            <label for="inputEmail" className="col-sm-1 col-form-label atributos">
-              <b>Correo</b>
-            </label>
-            <div className="col-sm-4">
-              <input
-                type="email"
-                className="form-control"
-                id="inputEmail"
-                placeholder="name@example.com"
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-              />
-            </div>
-
-            <div className="col-3"></div>
-          </div>
-          <div className="row ">
-            <div className="col-lg-4 col-sm-2"></div>
-            <div className="col-lg-4 col-sm-6 ">
-            <button
-              type="button"
-              className="btn botonR fs-5 mt-4"
-              onClick={login}
-            >
-              <b>Enviar Correo</b>
-            </button>
-            </div>
-            <div className="col-lg-4 col-sm-2"></div>
-          </div>
-        </div>
-      </>
-    );
+      });
   };
-  
-  export default password;
+
+  return (
+    <div className="Login">
+      <div className="row d-flex justify-content-center">
+        <h1 className="d-flex justify-content-center titulo "></h1>
+      </div>
+
+      <div className="mb-3 row">
+        <div className="col-4"></div>
+        <label htmlFor="inputEmail" className="col-sm-1 col-form-label atributos">
+          <b>Correo</b>
+        </label>
+        <div className="col-sm-4">
+          <input
+            type="email"
+            className="form-control"
+            id="inputEmail"
+            placeholder="name@example.com"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+
+        <div className="col-3"></div>
+      </div>
+      <div className="row ">
+        <div className="col-lg-4 col-sm-2"></div>
+        <div className="col-lg-4 col-sm-6 ">
+          <button
+            type="button"
+            className="btn botonR fs-5 mt-4"
+            onClick={login}
+          >
+            <b>Enviar Correo</b>
+          </button>
+        </div>
+        <div className="col-lg-4 col-sm-2"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Password;
